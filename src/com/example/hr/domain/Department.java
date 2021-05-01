98c2d862adf0732144dfc1eaa6a9d620fdad4ee3
Erhan Hepyasar <erhan.hepyasar@gmail.com>
2021-05-01 14:29:57 +0300

    Initial commit

---
+++ src/com/example/hr/domain/Department.java
@@ -0,0 +1,4 @@
+package com.example.hr.domain;
+
+public class Department {
+}
---
+++ src/com/example/hr/domain/Employee.java
@@ -0,0 +1,14 @@
+package com.example.hr.domain;
+
+// Entity class -> Identity
+public class Employee {
+    private TcKimlikNo identity;
+    private FullName fullname;
+    private Money salary;
+    private Iban iban;
+    private BirthYear birthYear;
+    private Department department;
+    private JobType jobType;
+    private Photo photo;
+
+}
